e2e38785cb67478a20c91174fe88709c8b71c36b
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-10-13 19:42:49 +0530

    Book Appointment App can add new users more than one user in local storage

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var form = document.querySelector("form"); // Select the form element
+    var form = document.querySelector("form");
 
     form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevent the form from submitting initially
+        event.preventDefault();
 
-        // Get form input values
         var username = document.getElementById("username").value;
         var email = document.getElementById("email").value;
         var phoneNumber = document.getElementById("phoneNumber").value;
 
-        // Validate form fields (rest of your validation code)
-
-        // If all validations pass, store the form data in local storage
         if (username && email && phoneNumber) {
-            var formData = {
+            // Get existing user data from local storage (if any)
+            var existingData = JSON.parse(localStorage.getItem("formData")) || [];
+
+            // Create a new user object
+            var newUser = {
                 username: username,
                 email: email,
                 phoneNumber: phoneNumber
             };
 
-            // Store the data in local storage as JSON
-            localStorage.setItem("formData", JSON.stringify(formData));
+            // Make sure `existingData` is an array
+            if (!Array.isArray(existingData)) {
+                existingData = [];
+            }
 
-            // You can provide a success message or redirect to another page here
+            // Add the new user to the existing data array
+            existingData.push(newUser);
+
+            // Store the updated data in local storage
+            localStorage.setItem("formData", JSON.stringify(existingData));
+
             alert("Form data has been stored in local storage.");
 
-            // Optionally, clear the form
             form.reset();
         } else {
             alert("Please fill in all required fields.");
