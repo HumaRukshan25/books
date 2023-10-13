9f620e11186f1205bd1cd0c135a2db5976ea39d8
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-10-13 13:39:46 +0530

    commit it

--- app.js
+++ app.js
@@ -20,3 +20,12 @@ for (var i = 0; i < elementsWithNewClass.length; i++) {
 var liElements = ul.getElementsByTagName('li');
 var lastLi = liElements[liElements.length - 1]; // Get the last <li> element
 lastLi.style.fontWeight = 'bold'; // Change the font weight
+// Change the font color to green for the 2nd item in the item list
+var secondItem = document.querySelector('#items li:nth-child(2)');
+secondItem.style.color = 'green';
+
+// Make all odd elements' background green
+var oddItems = document.querySelectorAll('#items li:nth-child(odd)');
+oddItems.forEach(function(item) {
+  item.style.backgroundColor = 'green';
+});
