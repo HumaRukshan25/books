fda7e4e3040a82d59397c6d4cb19cec2138f4837
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-10-13 20:06:55 +0530

    Delete data from local storage

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
     var form = document.querySelector("form");
 
+    // Function to delete a contact
+    function deleteContact(index) {
+        // Get the existing contact list from local storage
+        var existingContacts = JSON.parse(localStorage.getItem("formData")) || [];
+
+        // Check if the index is valid
+        if (index >= 0 && index < existingContacts.length) {
+            // Remove the contact from the array
+            existingContacts.splice(index, 1);
+
+            // Update the data in local storage
+            localStorage.setItem("formData", JSON.stringify(existingContacts));
+
+            // Refresh the contact list in the UI
+            displayContactList(existingContacts);
+        }
+    }
+
+    // Function to display the contact list in the UI
+    function displayContactList(contacts) {
+        var userList = document.getElementById("user-list");
+        userList.innerHTML = ""; // Clear the existing list
+
+        // Iterate through the contacts and display them
+        contacts.forEach(function (contact, index) {
+            var userItem = document.createElement("li");
+            userItem.classList.add("user-item");
+            userItem.innerHTML =
+                `<div class="user-info">` +
+                `<span>Name: ${contact.username}</span>` +
+                `<span>Email: ${contact.email}</span>` +
+                `<span>Phone: ${contact.phoneNumber}</span>` +
+                `</div>` +
+                `<button class="delete-button" data-index="${index}">Delete</button>`;
+
+            userList.appendChild(userItem);
+        });
+    }
+
+    // Event listener for form submission
     form.addEventListener("submit", function (event) {
         event.preventDefault();
 
@@ -9,32 +49,35 @@ document.addEventListener("DOMContentLoaded", function () {
         var phoneNumber = document.getElementById("phoneNumber").value;
 
         if (username && email && phoneNumber) {
-            // Get existing user data from local storage (if any)
-            var existingData = JSON.parse(localStorage.getItem("formData")) || [];
-
-            // Create a new user object
             var newUser = {
                 username: username,
                 email: email,
                 phoneNumber: phoneNumber
             };
 
-            // Make sure `existingData` is an array
-            if (!Array.isArray(existingData)) {
-                existingData = [];
-            }
+            var existingContacts = JSON.parse(localStorage.getItem("formData")) || [];
+            existingContacts.push(newUser);
 
-            // Add the new user to the existing data array
-            existingData.push(newUser);
+            localStorage.setItem("formData", JSON.stringify(existingContacts));
 
-            // Store the updated data in local storage
-            localStorage.setItem("formData", JSON.stringify(existingData));
-
-            alert("Form data has been stored in local storage.");
+            // Refresh the contact list in the UI
+            displayContactList(existingContacts);
 
             form.reset();
         } else {
             alert("Please fill in all required fields.");
         }
     });
+
+    // Event delegation for delete buttons
+    document.getElementById("user-list").addEventListener("click", function (event) {
+        if (event.target.classList.contains("delete-button")) {
+            var index = event.target.getAttribute("data-index");
+            deleteContact(index);
+        }
+    });
+
+    // Initial display of the contact list
+    var initialContacts = JSON.parse(localStorage.getItem("formData")) || [];
+    displayContactList(initialContacts);
 });
