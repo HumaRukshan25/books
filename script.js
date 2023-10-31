ae494107f208d8203f373fb05edb666893cd7b51
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-10-31 12:51:54 +0530

    delete and update feature added

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     var form = document.querySelector("form");
+    var userList = document.getElementById("user-list");
+    var usersData = [];
 
     function deleteContact(index) {
-        // Implement your delete contact logic
+        var deletedUser = usersData[index];
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + deletedUser.id)
+            .then(function (response) {
+                console.log('User data deleted:', response.data);
+                usersData.splice(index, 1);
+                displayContactList(usersData);
+            })
+            .catch(function (error) {
+                console.error('Error deleting user data:', error);
+            });
     }
 
     function displayContactList(contacts) {
-        var userList = document.getElementById("user-list");
         userList.innerHTML = "";
 
         contacts.forEach(function (contact, index) {
@@ -18,7 +28,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 `<span>Email: ${contact.email}</span>` +
                 `<span>Phone: ${contact.phoneNumber}</span>` +
                 `</div>` +
-                `<button class="delete-button" data-index="${index}">Delete</button>`;
+                `<button class="delete-button" data-index="${index}">Delete</button>` +
+                `<button class="update-button" data-index="${index}">Update</button>`;
 
             userList.appendChild(userItem);
         });
@@ -38,12 +49,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 phoneNumber: phoneNumber
             };
 
-            // Send the user data to the server (test endpoint provided by JSONPlaceholder)
             axios.post('https://jsonplaceholder.typicode.com/posts', newUser)
                 .then(function (response) {
                     console.log('User data sent to the server:', response.data);
-                    // Refresh the contact list in the UI
-                    displayContactList([newUser]);
+                    newUser.id = response.data.id;
+                    usersData.push(newUser);
+                    displayContactList(usersData);
                     form.reset();
                 })
                 .catch(function (error) {
@@ -54,10 +65,38 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    document.getElementById("user-list").addEventListener("click", function (event) {
+    userList.addEventListener("click", function (event) {
         if (event.target.classList.contains("delete-button")) {
             var index = event.target.getAttribute("data-index");
             deleteContact(index);
+        } else if (event.target.classList.contains("update-button")) {
+            var index = event.target.getAttribute("data-index");
+            var existingUser = usersData[index];
+            document.getElementById("username").value = existingUser.username;
+            document.getElementById("email").value = existingUser.email;
+            document.getElementById("phoneNumber").value = existingUser.phoneNumber;
+            document.getElementById("edit-user-index").value = index;
+            document.getElementById("update-button").style.display = "block";
         }
+    });
+
+    document.getElementById("update-button").addEventListener("click", function () {
+        var index = document.getElementById("edit-user-index").value;
+        var updatedUser = usersData[index];
+        updatedUser.username = document.getElementById("username").value;
+        updatedUser.email = document.getElementById("email").value;
+        updatedUser.phoneNumber = document.getElementById("phoneNumber").value;
+
+        axios.put('https://jsonplaceholder.typicode.com/posts/' + updatedUser.id, updatedUser)
+            .then(function (response) {
+                console.log('User data updated:', response.data);
+                usersData[index] = updatedUser;
+                displayContactList(usersData);
+                form.reset();
+                document.getElementById("update-button").style.display = "none";
+            })
+            .catch(function (error) {
+                console.error('Error updating user data:', error);
+            });
     });
 });
